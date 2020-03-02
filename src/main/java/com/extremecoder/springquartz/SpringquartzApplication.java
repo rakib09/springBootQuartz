d76e3ac487514b95d5873432bb53ee2caab5a4b6
Rakib Hasan <rakib.hasan786@gmail.com>
2020-03-03 00:27:43 +0600

    Initiate Project

---
+++ src/main/java/com/extremecoder/springquartz/SpringquartzApplication.java
@@ -0,0 +1,13 @@
+package com.extremecoder.springquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringquartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringquartzApplication.class, args);
+    }
+
+}
